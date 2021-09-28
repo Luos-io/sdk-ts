7696df6c28db26706cfe6f699ae161c7362d726e
Benjamin Christau <benjamin.christau@luos.io>
2021-09-28 16:02:21 +0200

    Handle "onData" event to send back sniffer data to SDK caller

--- src/inspect/_host.ts
+++ src/inspect/_host.ts
@@ -2,10 +2,10 @@ import { logger } from '@luos-io/utils';
 import SerialPort from 'serialport';
 import { defaultSerialOptions, serialPortUtils } from 'utils/index';
 
-import type { IInspect } from './interfaces';
+import type { IInspect, InspectData } from './interfaces';
 
 export const hostInspect: IInspect = async (path, options) => {
-  const { debug } = options;
+  const { debug, onData } = options;
 
   const port = new SerialPort(path, {
     ...defaultSerialOptions,
@@ -38,7 +38,6 @@ export const hostInspect: IInspect = async (path, options) => {
             debug,
           );
           if (hasWriteStartSucceed) {
-            console.log('started');
             const parser: SerialPort.parsers.Readline = port.pipe(
               new SerialPort.parsers.Readline({
                 delimiter: [0x7e, 0x7e, 0x7e],
@@ -46,26 +45,48 @@ export const hostInspect: IInspect = async (path, options) => {
               }),
             );
 
-            await new Promise((res, _rej) => {
+            await new Promise<InspectData>((res, _rej) => {
               parser.on('data', (data) => {
                 const timestamp = Buffer.from(data.slice(0, 16), 'hex')
                   .reverse()
                   .reduce((acc, b) => (acc = (acc << 8) + b), 0);
-                const protocol = data.slice(17, 18);
-                const target = data.slice(18, 20) + data.slice(16, 17);
-                const target_mode = data.slice(21, 22);
-                const source = data.slice(22, 24) + data.slice(20, 21);
-                const cmd = data.slice(24, 26);
-                const size = data.slice(28, 30) + data.slice(26, 28);
-                const size_num = parseInt(size, 16);
+                const protocol = parseInt(data.slice(17, 18));
+                const target = parseInt(
+                  data.slice(18, 20) + data.slice(16, 17),
+                );
+                const target_mode = parseInt(data.slice(21, 22));
+                const source = parseInt(
+                  data.slice(22, 24) + data.slice(20, 21),
+                );
+                const cmd = parseInt(data.slice(24, 26));
+                const size = parseInt(
+                  data.slice(28, 30) + data.slice(26, 28),
+                  16,
+                );
 
-                let msg: string | number;
-                if (size_num > 128) {
+                let msg: string;
+                if (size > 128) {
                   msg = data.slice(30, 30 + 128 * 2);
-                } else if (size_num === 0) {
-                  msg = 0;
+                } else if (size === 0) {
+                  msg = '';
                 } else {
-                  msg = data.slice(30, 30 + size_num * 2);
+                  msg = data.slice(30, 30 + size * 2);
+                }
+
+                // TO DO - Save data
+
+                // Send back data
+                if (onData && typeof onData === 'function') {
+                  onData({
+                    timestamp,
+                    protocol,
+                    target,
+                    target_mode,
+                    source,
+                    cmd,
+                    size,
+                    msg,
+                  });
                 }
 
                 res({
--- src/inspect/interfaces.ts
+++ src/inspect/interfaces.ts
@@ -1,3 +1,14 @@
+export interface InspectData {
+  timestamp: number;
+  protocol: number;
+  target: number;
+  target_mode: number;
+  source: number;
+  cmd: number;
+  size: number;
+  msg: string;
+}
+
 export interface InspectCallback {
   (): Promise<boolean>;
 }
@@ -5,6 +16,7 @@ export interface InspectCallback {
 export type InspectOptions = {
   debug: boolean;
   time: number;
+  onData: (data: InspectData) => void;
 };
 
 export interface IInspect {
